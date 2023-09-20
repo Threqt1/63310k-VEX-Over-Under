8817fba32d9cc03175b92124e6619f76c202ee5d
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2023-09-19 21:36:03 -0500

    switched to l1 l2

--- src/opcontrol/mechanisms.cpp
+++ src/opcontrol/mechanisms.cpp
@@ -6,10 +6,15 @@ void MechanismsLoop(void *_)
 
     while (true)
     {
-        if (controller.getDigital(ControllerDigital::A))
+        if (controller.getDigital(ControllerDigital::L1))
         {
             liftStopped = false;
             lift.moveVoltage(10000);
+        }
+        else if (controller.getDigital(ControllerDigital::L2))
+        {
+            liftStopped = false;
+            lift.moveVoltage(-10000);
         }
         else if (!liftStopped)
         {
