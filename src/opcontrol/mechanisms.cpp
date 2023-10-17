c3670067cd8d471491a1a954785485eeef21fb5b
KevDoesntDev <82847530+KevDoesntDev@users.noreply.github.com>
2023-10-16 19:50:47 -0500

    a

--- src/opcontrol/mechanisms.cpp
+++ src/opcontrol/mechanisms.cpp
@@ -6,20 +6,23 @@ void MechanismsLoop(void *_)
 
     while (true)
     {
-        if (controller.getDigital(ControllerDigital::L1))
-        {
+        if (controller.getDigital(ControllerDigital::L1)) {
             liftStopped = false;
-            lift.moveVoltage(7500);
-        }
-        else if (controller.getDigital(ControllerDigital::L2))
-        {
+            //lift.moveVoltage(12000);
+            lift.moveVelocity(70);
+        } else if (controller.getDigital(ControllerDigital::L2)) {
             liftStopped = false;
-            lift.moveVoltage(-7500);
-        }
-        else if (!liftStopped)
-        {
+            //lift.moveVoltage(-12000);
+            lift.moveVelocity(-70);
+        } else if (!liftStopped) {
             liftStopped = true;
             lift.moveVoltage(0);
+        }
+
+        if (controller.getDigital(ControllerDigital::B)) {
+            lift.moveVelocity(-70);
+        } else if (controller.getDigital(ControllerDigital::A)) {
+            lift.moveVelocity(0);
         }
 
         if (controller.getDigital(ControllerDigital::R2)) {
@@ -27,7 +30,6 @@ void MechanismsLoop(void *_)
         } else if (controller.getDigital(ControllerDigital::R1)) {
             piston.set_value(false);
         }
-
         pros::delay(20);
     }
 }
